e9c9389476f9ca81164aa05126f1930e57c1c5f3
BillyYccc <billy112487983@gmail.com>
2017-10-22 00:05:52 +0800

    Init the project skeleton

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # vertx-postgresql-starter
+
+This is a starter to build a RESTful Web Service with [Vert.x](http://vertx.io/) stack and [postgreSQL](https://www.postgresql.org/).
+It leverages [Gradle](gradle.org) to build and [Docker](https://www.docker.com/) to boost deployment.
+
+[![Build Status](https://travis-ci.org/BillyYccc/vertx-postgresql-starter.svg?branch=master)](https://travis-ci.org/BillyYccc/vertx-postgresql-starter)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/300d3703677b4cc3ace9e30ef6438586)](https://www.codacy.com/app/BillyYccc/vertx-postgresql-starter?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=BillyYccc/vertx-postgresql-starter&amp;utm_campaign=Badge_Grade)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/BillyYccc/vertx-postgresql-starter/blob/master/LICENSE)
+
+
+
---
+++ src/test/java/com/billyyccc/MainVerticleTest.java
@@ -0,0 +1,66 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Billy Yuan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.billyyccc;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Billy Yuan <billy112487983@gmail.com>
+ */
+
+@RunWith(VertxUnitRunner.class)
+public class MainVerticleTest {
+  private Vertx vertx;
+
+  @Before
+  public void setUp(TestContext testContext) {
+    vertx = Vertx.vertx();
+    vertx.deployVerticle(MainVerticle.class.getName(), testContext.asyncAssertSuccess());
+  }
+
+  @After
+  public void tearDown(TestContext testContext) {
+    vertx.close(testContext.asyncAssertSuccess());
+  }
+
+  @Test
+  public void testServer(TestContext testContext) {
+    Async async = testContext.async();
+    vertx.createHttpClient().getNow(8080, "localhost", "/", res -> {
+      testContext.assertEquals(res.statusCode(), 200);
+      res.bodyHandler(body -> {
+        testContext.assertTrue(body.length()>0);
+        async.complete();
+      });
+    });
+  }
+}
